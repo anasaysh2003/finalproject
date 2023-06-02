b8dcb62099dacc00fd2591dcdadd1e8c034d3fc8
anasaysh2003 <135332265+anasaysh2003@users.noreply.github.com>
2023-06-02 14:45:57 +0300

    api2.php

---
+++ api2.php
@@ -0,0 +1,11 @@
+<?php
+include 'db_helper.php';
+header("Content-Type: application/json; charset=UTF-8");
+$dbHelper = new DbHelper();
+$dbHelper->createDbConnection();
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $dbHelper->insertNewStudent($name,$email);
+}
+?>
